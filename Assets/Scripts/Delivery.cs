5e3eddc83215df5eda7950d5f9954d79c05e625b
Adrián Bornás Alcázar <adrian.bornas@gmail.com>
2022-05-17 21:51:45 +0200

    Only deliver a package when is picked up

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    private bool hasPackage = false;
+
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
     void OnTriggerEnter2D(Collider2D other) {
         if ("Package" == other.tag) {
             Debug.Log("Package picked up");
+            hasPackage = true;
         }
 
-        if ("Customer" == other.tag) {
+        if ("Customer" == other.tag && hasPackage) {
             Debug.Log("Delivered package");
+            hasPackage = false;
         }
     }
 }
