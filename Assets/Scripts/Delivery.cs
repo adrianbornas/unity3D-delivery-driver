8e1bdfc93ae102a6830ecf38b3631ee329f32fdb
Adrián Bornás Alcázar <adrian.bornas@gmail.com>
2022-05-24 23:35:47 +0200

    Make the car change color depending if it has package or not. Make packages and customer to have the same color as car states

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    private bool hasPackage = false;
+    SpriteRenderer spriteRenderer;
 
     [SerializeField] float destroyDelay = 0.5f;
+     
+    bool hasPackage = false;
+    [SerializeField] Color32 hasPackageColor = new Color32(14, 176, 4, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(166, 50, 50, 255);
 
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = noPackageColor;
+    }
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
@@ -15,6 +23,7 @@ public class Delivery : MonoBehaviour
         if ("Package" == other.tag && !hasPackage) {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
 
             Destroy(other.gameObject, destroyDelay);
         }
@@ -22,6 +31,7 @@ public class Delivery : MonoBehaviour
         if ("Customer" == other.tag && hasPackage) {
             Debug.Log("Delivered package");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
